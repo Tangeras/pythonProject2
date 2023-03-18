ae0496a927d7b3224d05117829965ba2327efcdb
Tangeras <baxter76@inbox.ru>
2023-03-18 13:38:17 +0300

    Задание 13.4.8

--- 13.4.8.py
+++ 13.4.8.py
@@ -1,8 +1,10 @@
- try
+ try:
     str_1=int(input("Строка 1"))
     str_2=int(input("Строка 2"))
-    str_3=int(input("Строка 3"))
-except ValueError as error
+    str_3=int(input("Строка 3")):
+except ValueError as error:
  print(error)
-else print("Вы ввели числа:", str_1, str_2,str_3)
-finally print("выход их программы")
+else:
+     print("Вы ввели числа:", str_1, str_2,str_3)
+finally:
+     print("выход их программы")
