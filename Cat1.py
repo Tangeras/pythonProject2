f51ec5b6d7d66bac9f1e22a88988f58b3f198b84
Tangeras <baxter76@inbox.ru>
2023-04-12 22:26:11 +0300

    Add files

---
+++ Cat1.py
@@ -0,0 +1,42 @@
+
+class Cat1:
+    def __init__(self, name, gender, age):
+        self.name = name
+        self.gender = gender
+        self.age = age
+
+    def getName(self):
+             return self.name
+
+    def getGender(self):
+             return self.gender
+
+    def getAge(self):
+             return self.age
+
+
+
+
+    # def init_from_dict(self,cat):
+    #         self.name=cat.get("name")
+    #         self.gender=cat.get("gender")
+    #         self.age=cat.get("age")
+    #              cats = [
+    #              {"name": "Baron",
+    #              "gender": "boy",
+    #              "age": 2,
+    #              },
+    #             {"name": "Sam",
+    #              "gender": "boy",
+    #              "age": 2,
+    #              },
+    #                ]
+    #      for cat in cats:
+    #            cat_obj = Cat1(name=cat.get("name"),
+    #                         gender=cat.get("gender"),
+    #                         age=cat.get("age"))
+    #            print(cat_obj.name)
+
+
+
+
