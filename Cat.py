392464ed93675413bcd1a530fbe212d9a747f08e
Tangeras <baxter76@inbox.ru>
2023-04-12 22:25:58 +0300

    Add files

---
+++ Cat.py
@@ -0,0 +1,9 @@
+from Cat1 import Cat1
+Baron=Cat1("Барон", "мальчик", 2)
+Sam=Cat1("Сэм", "мальчик", 2)
+print(f'Кот {Baron.name}, пол-{Baron.gender}, возраст-{Baron.age}года')
+
+print(f'Кот {Sam.name}, пол-{Sam.gender}, возраст-{Sam.age}года')
+
+
+
